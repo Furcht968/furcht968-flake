9f530590e5572eccd432b4b6017870b33105d69b
furcht968 <furcht968@gmail.com>
2026-04-14 11:23:45 +0900

    edit: nomacsゴミすぎだしImageGlassないしでeog入れた

--- host/gui-apps.nix
+++ host/gui-apps.nix
@@ -2,7 +2,7 @@
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
-    firefox vscode vlc gedit obs-studio vesktop obsidian thunderbird pcmanfm nomacs
+    firefox vscode vlc gedit obs-studio vesktop obsidian thunderbird pcmanfm eog
     inkscape blender
   ];
 
